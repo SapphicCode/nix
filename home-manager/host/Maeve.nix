e9aed80dc4fc02f75575894b2db9dd6fd722eeae
Cassandra <git@sapphicco.de>
2024-03-19 13:47:14 +0100

    hm/Maeve: ollama service

--- home-manager/host/Maeve.nix
+++ home-manager/host/Maeve.nix
@@ -12,6 +12,17 @@
 
   services.syncthing.enable = true;
 
+  launchd.agents.ollama = {
+    enable = true;
+    config = {
+      ProgramArguments = ["${pkgs.ollama}/bin/ollama" "serve"];
+      KeepAlive = {
+        Crashed = true;
+        SuccessfulExit = false;
+      };
+    };
+  };
+
   imports = [
     ../profile/everything.nix
   ];
