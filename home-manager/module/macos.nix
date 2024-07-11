62b87b5856bcc69d456ba39cba1beda9b3e61835
Cassandra <git@sapphicco.de>
2024-07-11 22:22:23 +0200

    hm/macos: switch to uutils

--- home-manager/module/macos.nix
+++ home-manager/module/macos.nix
@@ -2,11 +2,11 @@
   home.packages = with pkgs; [
     _1password
     bash
-    coreutils
     curl
     gnugrep
     gnused
     lima
     qemu # podman machine
+    uutils-coreutils-noprefix
   ];
 }
