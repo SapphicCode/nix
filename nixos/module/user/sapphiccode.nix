b1e7773bb482c54fdcd32fc9bd546c2bc7e3fe2d
Cassandra <git@sapphicco.de>
2024-07-11 22:22:10 +0200

    nixos/user/sapphiccode: move back to fish because we don't have to appease gnome

--- nixos/module/user/sapphiccode.nix
+++ nixos/module/user/sapphiccode.nix
@@ -3,7 +3,7 @@
     isNormalUser = true;
     description = "Cassandra";
     extraGroups = ["wheel" "networkmanager" "input" "lp" "scanner" "dialout"];
-    shell = pkgs.bash;
+    shell = pkgs.fish;
   };
   programs.fish.enable = true;
   programs._1password-gui.polkitPolicyOwners = ["sapphiccode"];
