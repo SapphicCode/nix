5daabab3733043274a4eae6d82d959ab7acd60e3
Cassandra <git@sapphicco.de>
2024-08-15 17:33:57 +0200

    style(nixos/server): alejandra

--- nixos/profile/server.nix
+++ nixos/profile/server.nix
@@ -100,21 +100,21 @@
     wants = ["network-online.target"];
     after = ["network-online.target"];
     script = ''
-    set -euxo pipefail
+      set -euxo pipefail
 
-    generated_excludes=$(mktemp)
-    ${pkgs.fd}/bin/fd --absolute-path --hidden --full-path 'containers/storage/volumes/([0-9a-f]{64}|\w+_cache)$' /var/lib /home > $generated_excludes
+      generated_excludes=$(mktemp)
+      ${pkgs.fd}/bin/fd --absolute-path --hidden --full-path 'containers/storage/volumes/([0-9a-f]{64}|\w+_cache)$' /var/lib /home > $generated_excludes
 
-    ${pkgs.restic}/bin/restic backup \
-      --exclude-caches \
-      --exclude-file $generated_excludes \
-      --exclude /var/lib/containers/storage/overlay \
-      --exclude '/home/*/.local/share/containers/storage/overlay' \
-      --exclude '/home/*/.cache' \
-      --exclude '/home/*/Downloads' \
-      /etc \
-      /home \
-      /var/lib
+      ${pkgs.restic}/bin/restic backup \
+        --exclude-caches \
+        --exclude-file $generated_excludes \
+        --exclude /var/lib/containers/storage/overlay \
+        --exclude '/home/*/.local/share/containers/storage/overlay' \
+        --exclude '/home/*/.cache' \
+        --exclude '/home/*/Downloads' \
+        /etc \
+        /home \
+        /var/lib
     '';
     serviceConfig.EnvironmentFile = "/etc/creds/restic.env";
     serviceConfig.Environment = "HOME=%h";
