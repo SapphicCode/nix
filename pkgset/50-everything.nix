8639ef682ebda9f13fb463686b59f77ae8e22138
Cassandra <git@sapphicco.de>
2024-12-28 00:26:13 +0100

    fix: use brew's ffmpeg

--- pkgset/50-everything.nix
+++ pkgset/50-everything.nix
@@ -2,39 +2,44 @@
   pkgs,
   fallback ? pkgs,
 }:
-with pkgs; [
-  # more shells!
-  xonsh
+with pkgs;
+  [
+    # more shells!
+    xonsh
 
-  # all remaining (occasional use) tools
-  mods
-  ollama
-  gh
-  nb
-  yt-dlp
-  jwt-cli
-  fossil
-  pipx # in case of fire break glass
-  jujutsu
-  taskwarrior3
-  taskwarrior-tui
-  yubikey-manager
+    # all remaining (occasional use) tools
+    mods
+    ollama
+    gh
+    nb
+    yt-dlp
+    jwt-cli
+    fossil
+    pipx # in case of fire break glass
+    jujutsu
+    taskwarrior3
+    taskwarrior-tui
+    yubikey-manager
 
-  # transcoding / image manipulation
-  ffmpeg
-  imagemagickBig
-  exiftool
-  libjxl
-  imagemagickBig
+    # transcoding / image manipulation
+    imagemagickBig
+    exiftool
+    libjxl
+    imagemagickBig
 
-  # cloud utils
-  # awscli2 # (broken)
+    # cloud utils
+    # awscli2 # (broken)
 
-  # programming languages in global context
-  python312
-  python312Packages.ipython
-  go
+    # programming languages in global context
+    python312
+    python312Packages.ipython
+    go
 
-  # other programming language tooling
-  stylua
-]
+    # other programming language tooling
+    stylua
+  ]
+  ++ (
+    if pkgs.stdenv.isLinux
+    then with pkgs; [ffmpeg]
+    else []
+  )
