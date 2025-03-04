9f5863c5a9e6b0bf54d1c0b4051a139915724a79
Cassandra <git@sapphicco.de>
2025-03-04 12:58:09 +0100

    feat(nix-darwin/clementine): add trusted-users

--- nix-darwin/host/clementine.nix
+++ nix-darwin/host/clementine.nix
@@ -6,5 +6,6 @@
   nixpkgs.hostPlatform = "aarch64-darwin";
   services.nix-daemon.enable = true;
   nix.package = unstable.lix;
+  nix.settings.trusted-users = ["@staff"];
   system.stateVersion = 5;
 }
