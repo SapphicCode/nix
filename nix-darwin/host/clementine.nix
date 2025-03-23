fd0f5a16aef421f01f8a08cc424b24ca829c44e6
Cassandra <git@sapphicco.de>
2025-03-23 09:47:07 +0100

    fix(nix-darwin/clementine): trusted-users

--- nix-darwin/host/clementine.nix
+++ nix-darwin/host/clementine.nix
@@ -6,6 +6,6 @@
   nixpkgs.hostPlatform = "aarch64-darwin";
   services.nix-daemon.enable = true;
   nix.package = unstable.lix;
-  nix.settings.trusted-users = ["@staff"];
+  nix.settings.trusted-users = ["sapphiccode"];
   system.stateVersion = 5;
 }
