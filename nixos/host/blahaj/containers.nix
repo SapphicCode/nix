4b735171c4d0452652fe28939f5f5cd5d68edba4
Cassandra <git@sapphicco.de>
2025-04-04 20:28:40 +0200

    fix(nixos/blahaj): disable archivebox for now

--- nixos/host/blahaj/containers.nix
+++ nixos/host/blahaj/containers.nix
@@ -7,7 +7,7 @@
     "containers/systemd/sapphiccode-cloudflare.container".source = ./containers/sapphiccode-cloudflare.container;
     "containers/systemd/sapphiccode-uptimekuma.container".source = ./containers/sapphiccode-uptimekuma.container;
     "containers/systemd/sapphiccode-pufferpanel.container".source = ./containers/sapphiccode-pufferpanel.container;
-    "containers/systemd/sapphiccode-archivebox.container".source = ./containers/sapphiccode-archivebox.container;
+    # "containers/systemd/sapphiccode-archivebox.container".source = ./containers/sapphiccode-archivebox.container;
     "containers/systemd/sapphiccode-bookstack.container".source = ./containers/sapphiccode-bookstack.container;
     "containers/systemd/sapphiccode-nextcloud.container".source = ./containers/sapphiccode-nextcloud.container;
     "containers/systemd/sapphiccode-attic.container".source = ./containers/sapphiccode-attic.container;
