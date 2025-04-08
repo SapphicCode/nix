7af54045557c21aa25a580ed0622bfe657edeb97
Cassandra <git@sapphicco.de>
2025-04-08 14:22:06 +0200

    nixos: fix disabled podman

--- nixos/module/podman.nix
+++ nixos/module/podman.nix
@@ -1,6 +1,9 @@
 {lib, ...}: {
   virtualisation.podman.enable = lib.mkDefault true;
   virtualisation.podman.dockerSocket.enable = true;
-  systemd.timers.podman-auto-update.wantedBy = ["timers.target"];
+  systemd.timers.podman-auto-update.wantedBy =
+    if config.virtualisation.podman.enable
+    then ["timers.target"]
+    else [];
   networking.firewall.trustedInterfaces = ["podman+"];
 }
