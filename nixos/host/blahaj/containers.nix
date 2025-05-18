6a973b673dfa8cfc4582095726d085bb4e962fd4
Cassandra <git@sapphicco.de>
2025-05-18 12:51:48 +0200

    nixos/blahaj: yeet nextcloud

--- nixos/host/blahaj/containers.nix
+++ nixos/host/blahaj/containers.nix
@@ -9,7 +9,6 @@
     "containers/systemd/sapphiccode-pufferpanel.container".source = ./containers/sapphiccode-pufferpanel.container;
     # "containers/systemd/sapphiccode-archivebox.container".source = ./containers/sapphiccode-archivebox.container;
     "containers/systemd/sapphiccode-bookstack.container".source = ./containers/sapphiccode-bookstack.container;
-    "containers/systemd/sapphiccode-nextcloud.container".source = ./containers/sapphiccode-nextcloud.container;
     "containers/systemd/sapphiccode-attic.container".source = ./containers/sapphiccode-attic.container;
 
     # CI/CD for Forgejo
