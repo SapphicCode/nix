361fcb79c68d39fae99431e274d37bd557f76586
Cassandra <git@sapphicco.de>
2026-04-04 19:39:12 +0200

    feat(nixos/bunker): enable postgresql 17 and kopia

--- nixos/host/bunker/default.nix
+++ nixos/host/bunker/default.nix
@@ -1,11 +1,17 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
     ../../module/systemd-boot.nix
     ../../module/systemd-networkd-en_dhcp.nix
     ../../profile/server_x86_64-linux.nix
+    ../../module/kopia.nix
   ];
 
   networking.hostName = "bunker";
   virtualisation.incus.agent.enable = true;
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_17;
+  };
 }
