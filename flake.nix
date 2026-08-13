4d35601cc42da5b617b0864533fbe643ba0c915c
Cassandra <git@sapphicco.de>
2026-08-13 10:18:17 +0200

    flake: update inputs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/97913488884f76f2c1925f79378e190957a9200a";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/232594377388c8653560d53a61583c8f3ef7bd76";
 
     flake-utils.url = "github:numtide/flake-utils";
 
